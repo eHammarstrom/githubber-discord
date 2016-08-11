120ff17f586f76678d6a244053dbb91f0e03153c
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2016-08-11 18:18:30 +0200

    message formatting

--- githubber-discord/Program.cs
+++ githubber-discord/Program.cs
@@ -128,7 +128,7 @@ public class Program {
 
                 toAnnounce.ToList().ForEach(async x => {
                     string msg = x.Author.Login + " has committed changes to " + repo.Name + ".\n" +
-                        "Message: " + x.Commit.Message + "\n" +
+                        "Message: " + x.Commit.Message + "\n\n" +
                         x.HtmlUrl;
                     await channel.SendMessage(msg);
                 });
